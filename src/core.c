f553570c83a4563a064622c8f745b12ff88e9ebf
Ryan Nelson <ryan@lillehammer.local>
2026-09-08 13:35:49 -0700

    Implement bounded unbuffered fread with element-count probes

--- src/core.c
+++ src/core.c
@@ -1516,6 +1516,7 @@ static const char *api_strerror(int error)
     case CB_ENAMETOOLONG: return "file name too long";
     case CB_ENOSYS: return "function not implemented";
     case CB_ENOTEMPTY: return "directory not empty";
+    case CB_EOVERFLOW: return "value too large to be stored in data type";
     default: return "unknown error";
     }
 }
